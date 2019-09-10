108ab13cd1076bc993b9da5aec08676c6239f013
lukehebb1 <33845020+lukehebb1@users.noreply.github.com>
2019-09-10 23:47:00 +0100

    Initial commit

---
+++ late.py
@@ -0,0 +1,16 @@
+from twilio.rest import Client
+import sys, os
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
+#create twilio object
+client = Client(account_sid, auth_token)
+
+my_num = '+12028583669'
+gf_num = '+353862259018'
+
+#create and send message
+message = client.messages.create(body="Hi, I'm really busy in college right now. I should be home by " + sys.argv[1], from_=my_num, to=gf_num)
+
+print(message.sid)
